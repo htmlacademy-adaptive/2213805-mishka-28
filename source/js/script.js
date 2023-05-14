6bd87562effb868bf0823a0026ddcd3b21772cd9
Olga Peganova <volha25121987@gmail.com>
2023-05-14 18:01:33 +0700

    modal

--- source/js/script.js
+++ source/js/script.js
@@ -17,3 +17,23 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+
+const modalWindow = document.querySelector('.modal');
+const cardOpenButton = document.querySelector('.catalog-card__button');
+const modalOpen = document.querySelector('.modal--open');
+const modalClose = document.querySelector('.modal--close');
+
+modalWindow.classList.remove('modal--close');
+modalWindow.classList.add('modal--open');
+
+cardOpenButton.addEventListener('click', function () {
+  modalOpen.classList.add('modal--open');
+  if (document.classList.contains('modal--close')) {
+    modalWindow.classList.remove('modal--close');
+    modalWindow.classList.add('modal--open');
+  } else {
+    modalWindow.classList.add('modal--close');
+    modalWindow.classList.remove('modal--open');
+  }
+});
